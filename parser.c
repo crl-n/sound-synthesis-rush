b574c395051f9c1e6a831e26a044e3bc8f5fc673
cnysten <cnysten@student.hive.fi>
2022-05-26 14:02:19 +0300

    parses tempo and track instruments

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: carlnysten <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 12:36:24 by carlnysten        #+#    #+#             */
-/*   Updated: 2022/05/26 13:03:52 by carlnysten       ###   ########.fr       */
+/*   Updated: 2022/05/26 14:01:28 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,74 @@
 t_song	parse(char *filename)
 {
 	t_song	song;
-
-	(void) filename;
-	bzero(&song, sizeof (t_song));
-
-	/*
 	FILE	*file;
 	size_t	sample_index = 0;
-	if (file = fopen(filename, "r")
+
+	bzero(&song, sizeof (t_song));
+	(void) sample_index;
+
+	if ((file = fopen(filename, "r")))
 	{
 		size_t	linesize = 0;
 		char	*line = NULL;
+		ssize_t	bytes_read = 0;
 
-		while (bytes_read = getline(line, &linesize, file))
+		// Get tempo
+		while ((bytes_read = getline(&line, &linesize, file)))
 		{
-			
+			if (strncmp(line, "tempo", 5) == 0)
+			{
+				song.tempo = atoi(line + 5);
+				free(line);
+				line = NULL;
+				break ;
+			}
+			free(line);
 		}
 		
+		// Get tracks
+		while ((bytes_read = getline(&line, &linesize, file)))
+		{
+			if (strncmp(line, "tracks", 6) == 0)
+			{
+				// Count how many tracks
+				int	n_tracks = 1;
+				for (int i = 0; line[i]; i++)
+				{
+					if (line[i] == ',')
+						n_tracks++;
+				}
+
+				// Get instruments
+				t_instrument	track_instruments[32] = {0};
+				int				trackid = 1;
+				char			*token = strtok(line, ",");
+				while (token != NULL)
+				{
+					if (!strcmp(line, "sine"))
+						track_instruments[trackid++] = sine;
+					else if (!strcmp(line, "saw"))
+						track_instruments[trackid++] = saw;
+					else if (!strcmp(line, "triangle"))
+						track_instruments[trackid++] = triangle;
+					else if (!strcmp(line, "square"))
+						track_instruments[trackid++] = square;
+					token = strtok(NULL, ",");
+				}
+				
+				free(line);
+				line = NULL;
+				break ;
+			}
+			free(line);
+			line = NULL;
+		}
 
 	}
 	else
 	{
 
 	}
-	*/
 	return (song);
 }
 
